b50dc6a271cb9d675842b0532281eb79f244ac99
EmilAhmaBoy <26015159-EmilAhmaBoy@users.noreply.replit.com>
2023-09-03 19:01:23 +0000

    0.2

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, info, forum, signin, signup
 
 urlpatterns = [
-    path('', index, name='index')
+  path('', index, name='index'),
+  path('info', info, name='info'),
+  path('forum', forum, name='forum'),
+  path('signin', signin, name='signin'),
+  path('signup', signup, name='signup'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+  return render(request, 'pages/index.html')
+
+def info(request):
+  return render(request, 'pages/info.html')
+
+def forum(request):
+  return render(request, 'pages/forum.html')
+
+def signin(request):
+  return render(request, 'pages/signin.html')
+
+def signup(request):
+  return render(request, 'pages/signup.html')
+
